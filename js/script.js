46cf0fa4a5361a74491a676e408ff9a9e83a3e35
MercurialMune <munenecyp@gmail.com>
2018-12-05 16:16:33 +0300

    perfect logic for all js functions

--- js/script.js
+++ js/script.js
@@ -3,21 +3,17 @@ function runScript() {
   var dim2 = document.getElementById('dim2').value;
   var dim3 = document.getElementById('dim3').value;
 
-  if (dim2 === 0||dim1 === ||dim3 ===0 || parseInt(dim1)===NaN || parseInt(dim2)===NaN || parseInt(dim3)===NaN) {
+  if (dim1 + dim2 <= dim3 || dim2 + dim3 <= dim1 || dim1 + dim3 <= dim2) {
     alert("Dimensions do not give a triangle!");
-  }
-  else if (dim2===dim3 && dim2!==dim1 && dim3!===dim1) {
+  } else if (dim1 !== 0 && dim2 !== 0 && dim3 !== 0) {
     alert("Dimensions do not give a triangle!");
-  }
-  else {
-    if (dim1 === dim2 && dim2 === dim3) {
-      alert("The triangle is Equilateral");
-    } else if (dim1 !== dim2 && dim2 === dim3 || dim2 === dim1 && dim2 !== dim3 || dim3 === dim1 && dim3 !== dim2) {
-      alert("The triangle is Isosceles");
-    } else if (dim2 !== dim3 && dim2 !== dim1 && dim1 !== dim3) {
-      alert(" The triangle is Scalene");
-    } else {
-      alert("Dimensions do not give a triangle!");
-    }
+  } else if (dim1 === dim2 && dim2 === dim3) {
+    alert("The triangle is Equilateral");
+  } else if (dim1 !== dim2 && dim2 === dim3 || dim2 === dim1 && dim2 !== dim3 || dim3 === dim1 && dim3 !== dim2) {
+    alert("The triangle is Isosceles");
+  } else if (dim2 !== dim3 && dim2 !== dim1 && dim1 !== dim3) {
+    alert(" The triangle is Scalene");
+  } else {
+    alert("Dimensions do not give a triangle!");
   }
 }
